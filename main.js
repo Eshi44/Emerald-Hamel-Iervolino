8caa6731797ffc331f312646ac351317d2ae63b0
Eshi44 <e.hameliervolino@gmail.com>
2020-04-12 16:58:16 -0400

    added media queries to css file for a mobile responsive design. edited the button's shadow. fixed the column divs on the first page

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#frontpage").attr("style","height:" + screen.height +"px");
-window.onresize = function(event) {
-    $("#frontpage").attr("style","height:" + screen.height + "px");
-    // screen.height
-}
-
-})
