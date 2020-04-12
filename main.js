11849a1eb32ddf6d3df4256139bdfc818caa1361
Eshi44 <e.hameliervolino@gmail.com>
2020-04-12 12:12:27 -0400

    created html, css, jss files. added an assets folder and img folder.

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#frontpage").attr("style","height:" + screen.height +"px");
+window.onresize = function(event) {
+    $("#frontpage").attr("style","height:" + screen.height + "px");
+    // screen.height
+}
+
+})
